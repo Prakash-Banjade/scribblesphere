53a66ff8bbb2f19aec2559ce12631a1c9fc834c3
Prakash Banjade <prakashbanjade191@gmail.com>
2023-06-22 10:29:19 +0545

    sending email, fullname from refreshToken controller

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -1,33 +1,36 @@
-const User = require('../model/User') 
+const User = require("../model/User");
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const handleRefreshToken = async (req, res) => {
-    const cookies = req.cookies;
-    if (!cookies?.jwt) return res.sendStatus(401);
-    const refreshToken = cookies.jwt;
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(401);
+  const refreshToken = cookies.jwt;
 
-    const foundUser = await User.findOne({refreshToken}).exec()
-    if (!foundUser) return res.sendStatus(403); //Forbidden 
-    const roles = Object.values(foundUser.roles)
-    // evaluate jwt 
-    jwt.verify(
-        refreshToken,
-        process.env.REFRESH_TOKEN_SECRET,
-        (err, decoded) => {
-            if (err || foundUser.email !== decoded.email) return res.sendStatus(403);
+  const foundUser = await User.findOne({ refreshToken }).exec();
+  if (!foundUser) return res.sendStatus(403); //Forbidden
+  const roles = Object.values(foundUser.roles);
+  // evaluate jwt
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    if (err || foundUser.email !== decoded.email) return res.sendStatus(403);
 
-            const accessToken = jwt.sign(
-                { userInfo: {
-                    "email": decoded.email,
-                    roles
-                } },
-                process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '30s' }
-            );
-            res.json({ accessToken, roles })
-        }
+    const accessToken = jwt.sign(
+      {
+        userInfo: {
+          email: decoded.email,
+          roles,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" }
     );
-}
+    res.json({
+      accessToken,
+      roles,
+      email: decoded.email,
+      fullname: foundUser.fullname,
+    });
+  });
+};
 
 module.exports = handleRefreshToken;
