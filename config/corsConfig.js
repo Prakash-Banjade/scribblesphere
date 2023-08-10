549ad74fa1e332535470489ca39465f316446216
Prakash Banjade <prakashbanjade191@gmail.com>
2023-08-10 20:19:57 +0545

    reset cors config back to normal

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -6,8 +6,14 @@ const frontEndUrl = [
 ];
 
 const corsOptions = {
-  origin: '*',
-  // credentials: true,
+  origin: (origin, callback) => {
+    if (frontEndUrl.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error(`${origin} blocked by CORS policy`));
+    }
+  },
+  credentials: true,
   optionsSuccessStatus: 200,
 };
 
