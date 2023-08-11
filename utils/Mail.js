e29d3c156a6781ca725c28e0d044aed3687c8b8d
Prakash Banjade <prakashbanjade191@gmail.com>
2023-08-12 00:26:01 +0545

    refactored node mailer configuration using async await & promises

--- utils/Mail.js
+++ utils/Mail.js
@@ -6,42 +6,60 @@ const OAuth2 = google.auth.OAuth2;
 const OAuth2_client = new OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, 'https://developers.google.com/oauthplayground')
 OAuth2_client.setCredentials({ refresh_token: process.env.CLIENT_REFRESH_TOKEN })
 
-function send_mail(name, recepient, otp) {
-    const accessToken = OAuth2_client.getAccessToken(); // generates new access token whenever the function is called
+async function send_mail(name, recepient, otp) {
+  const accessToken = OAuth2_client.getAccessToken(); // generates new access token whenever the function is called
 
-    const transport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: process.env.CLIENT_EMAIL,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            accessToken,
-            refreshToken: process.env.CLIENT_REFRESH_TOKEN,
-        }
-    })
-
-    const mail_options = {
-        from: `VERIFICATION ${process.env.CLIENT_EMAIL}`,
-        to: recepient,
-        subject: `${otp} is your ScribbleSphere registration code`,
-        html: html_message(name, otp),
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.CLIENT_EMAIL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      accessToken,
+      refreshToken: process.env.CLIENT_REFRESH_TOKEN,
     }
+  })
 
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transport.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
+  const mail_options = {
+    from: `VERIFICATION ${process.env.CLIENT_EMAIL}`,
+    to: recepient,
+    subject: `${otp} is your ScribbleSphere registration code`,
+    html: html_message(name, otp),
+  }
+
+  await new Promise((resolve, reject) => {
     transport.sendMail(mail_options, (err, result) => {
-        if (err) {
-            console.log('Error: ', err)
-        } else {
-            console.log('result:', result)
-        }
+      if (err) {
+        console.log('Error: ', err)
+        reject(err)
+      } else {
+        console.log('result:', result)
+        resolve(result)
+      }
 
-        transport.close();
+      transport.close();
     })
+  })
 
 }
 
 function html_message(user, otp) {
-    return `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+  return `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
     <div style="margin:50px auto;width:70%;padding:20px 0">
       <div style="border-bottom:1px solid #eee">
         <a href="https://scribblesphere.vercel.app" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600;color:#0bbe64;">ScribbleSphere</a>
